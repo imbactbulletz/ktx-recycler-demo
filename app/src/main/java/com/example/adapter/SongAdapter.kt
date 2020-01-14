1ec5c028815c3d1ff9a12358a0473268bb1fbda1
imbactbulletz <cveticstefancpl@gmail.com>
2020-01-15 00:46:39 +0100

    Added a proper example of listener

--- app/src/main/java/com/example/adapter/SongAdapter.kt
+++ app/src/main/java/com/example/adapter/SongAdapter.kt
@@ -17,10 +17,12 @@ class SongAdapter: ListAdapter<Song, SongAdapter.SongViewHolder>(SongAdapterDiff
         val view = LayoutInflater.from(parent.context).inflate(R.layout.song_list_item, parent, false)
         val viewHolder = SongViewHolder(view)
 
+        // aside from the obvious lack of encapsulation, listener should not be set out of view holder
+        // since accessing its itemView's ktx properties will cause them to be found by id (call findViewById)
+        // each time when on click callback executes
         view.setOnClickListener {
             // itemView and containerView references point to the same object
-            // listener will cause items to be looked up with findViewById each time, no matter
-            // if view holder has cache or not
+            // items will be looked up with findViewById each time, no matter if view holder has cache or not
 
             viewHolder.itemView.text_song_list_item_name.text = "This is a new song name!"
             // TextView var2 = (TextView)var10000.findViewById(id.text_song_list_item_name);
@@ -36,6 +38,21 @@ class SongAdapter: ListAdapter<Song, SongAdapter.SongViewHolder>(SongAdapterDiff
     }
 
     inner class SongViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+        init {
+            setProperOnClickListener()
+        }
+
+        // this function demonstrates proper setting of listener
+        private fun setProperOnClickListener() {
+            containerView.setOnClickListener {
+                // here the ktx views will actually be pulled from the cache
+                text_song_list_item_name.text = "This is a new name!"
+                // TextView var10000 = (TextView)SongViewHolder.this._$_findCachedViewById(id.text_song_list_item_name);
+                text_song_list_item_artist.text = "This is a new artist!"
+                // var10000 = (TextView)SongViewHolder.this._$_findCachedViewById(id.text_song_list_item_artist);
+            }
+        }
 
         fun onBind(song: Song) {
             // in order to make our find view cache get called we need to access ktx properties directly
