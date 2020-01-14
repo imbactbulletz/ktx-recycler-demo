83fd11b19d331ec53e2906c9f74e9d359f5b5c01
imbactbulletz <cveticstefancpl@gmail.com>
2020-01-14 22:21:04 +0100

    Created data set for recycler adapter

--- app/src/main/java/com/example/ktxrecyclerdemo/MainActivity.kt
+++ app/src/main/java/com/example/ktxrecyclerdemo/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.ktxrecyclerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.domain.Song
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val songList = createSongList()
+    }
+
+    fun createSongList(): List<Song> {
+        val songList = mutableListOf<Song>()
+        songList.add(Song("Right Here Right Now", "Fatboy Slim"))
+        songList.add(Song("Pjanoo", "Eric Prydz"))
+        songList.add(Song("Lola's Theme", "The Shapeshifters"))
+        songList.add(Song("Children", "Robert Miles"))
+        songList.add(Song("9pm [Till I Come]", "ATB"))
+        songList.add(Song("Greece 2000", "Three Drivers"))
+        songList.add(Song("Touch Me", "Rui Da Silva"))
+        songList.add(Song("You Don't Know Me", "Jax Jones"))
+        songList.add(Song("Galvanize", "The Chemical Brothers"))
+        songList.add(Song("At The River", "Groove Armada"))
+        songList.add(Song("Alright  ", "Red Carpet"))
+        songList.add(Song("Offshore", "Chicane"))
+        songList.add(Song("Rej", "Ame"))
+        return songList
     }
 }
