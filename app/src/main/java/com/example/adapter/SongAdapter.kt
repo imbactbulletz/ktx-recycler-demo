9500354cba213c9a1677ae08a4bf0b513b272e15
imbactbulletz <cveticstefancpl@gmail.com>
2020-01-15 00:40:45 +0100

    Example with listener

--- app/src/main/java/com/example/adapter/SongAdapter.kt
+++ app/src/main/java/com/example/adapter/SongAdapter.kt
@@ -15,16 +15,27 @@ import kotlinx.android.synthetic.main.song_list_item.view.*
 class SongAdapter: ListAdapter<Song, SongAdapter.SongViewHolder>(SongAdapterDiffUtilItemCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.song_list_item, parent, false)
-        return SongViewHolder(view)
+        val viewHolder = SongViewHolder(view)
+
+        view.setOnClickListener {
+            // itemView and containerView references point to the same object
+            // listener will cause items to be looked up with findViewById each time, no matter
+            // if view holder has cache or not
+
+            viewHolder.itemView.text_song_list_item_name.text = "This is a new song name!"
+            // TextView var2 = (TextView)var10000.findViewById(id.text_song_list_item_name);
+            viewHolder.containerView.text_song_list_item_artist?.text = "This is a new artist!"
+            //  var2 = (TextView)viewHolder.getContainerView().findViewById(id.text_song_list_item_artist);
+        }
+
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         holder.onBind(getItem(position))
     }
 
-    inner class SongViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), LayoutContainer {
-        override val containerView: View?
-        get() = itemView
+    inner class SongViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
 
         fun onBind(song: Song) {
             // in order to make our find view cache get called we need to access ktx properties directly
